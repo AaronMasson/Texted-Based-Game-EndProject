cc26b90a41965a03c65198ebafd708c44d86cfe9
AaronMasson <33638411+AaronMasson@users.noreply.github.com>
2017-11-13 15:30:02 -0500

    Initial commit

---
+++ Texted Based Game EndProject/ReadMe.txt
@@ -0,0 +1,40 @@
+========================================================================
+    CONSOLE APPLICATION : Texted Based Game EndProject Project Overview
+========================================================================
+
+AppWizard has created this Texted Based Game EndProject application for you.
+
+This file contains a summary of what you will find in each of the files that
+make up your Texted Based Game EndProject application.
+
+
+Texted Based Game EndProject.vcxproj
+    This is the main project file for VC++ projects generated using an Application Wizard.
+    It contains information about the version of Visual C++ that generated the file, and
+    information about the platforms, configurations, and project features selected with the
+    Application Wizard.
+
+Texted Based Game EndProject.vcxproj.filters
+    This is the filters file for VC++ projects generated using an Application Wizard. 
+    It contains information about the association between the files in your project 
+    and the filters. This association is used in the IDE to show grouping of files with
+    similar extensions under a specific node (for e.g. ".cpp" files are associated with the
+    "Source Files" filter).
+
+Texted Based Game EndProject.cpp
+    This is the main application source file.
+
+/////////////////////////////////////////////////////////////////////////////
+Other standard files:
+
+StdAfx.h, StdAfx.cpp
+    These files are used to build a precompiled header (PCH) file
+    named Texted Based Game EndProject.pch and a precompiled types file named StdAfx.obj.
+
+/////////////////////////////////////////////////////////////////////////////
+Other notes:
+
+AppWizard uses "TODO:" comments to indicate parts of the source code you
+should add to or customize.
+
+/////////////////////////////////////////////////////////////////////////////
---
+++ Texted Based Game EndProject/Texted Based Game EndProject.cpp
@@ -0,0 +1,113 @@
+// Texted Based Game 
+// Player will move around in a 10/10 array fighting enemies and finding items trying to get to the door. 
+//There are 5 floors to move through to get to the final boss.
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <Windows.h>
+
+using namespace std;
+// Prototype Functions
+void basement();
+void map();
+void displaymap(int row, int col, char board[10][10]);
+
+int main()
+{
+//====================== Aaron Masson ==============================
+	string name;
+	string answer;
+	bool valid = true;
+
+	cout << "Welcome to my program! " << endl;
+	cout << "Enter in your name: " << endl;
+	getline(cin, name);
+	cout << "Well " << name << ", get ready for a grand adventure through a dark and dangerous tower! " << endl;
+	cout << "Your prize is waiting at the top. Make Haste! " << endl;
+	cout << "To move around simply type go up, go down. Same is for left and right. " << endl;
+	cout << "Are you ready to embark on your journey? (yes) or (no) ";
+	cin >> answer;
+	while (answer != "no" && answer != "yes")
+	{
+		cout << "Enter in a valid answer: ";
+		cin >> answer;
+	}
+	if (answer == "yes")
+	{
+		cout << "Lets begin! " << endl;
+	
+		basement();
+	}
+
+	else if (answer == "no")
+	{
+		cout << "Too bad " << endl;
+		map();
+	}
+	
+
+	system("pause");
+    return 0;
+}
+
+void map()
+{
+	const int rows = 10;
+	const int cols = 10;
+
+	char board[rows][cols] = {
+		{' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' ,' ' },
+		{' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' ,' ' },
+		{' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' ,' ' },
+		{' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' ,' ' },
+		{' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' ,' ' },
+		{' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' ,' ' },
+		{' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' ,' ' },
+		{' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' ,' ' },
+		{' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' ,' ' },
+		{' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' ,' ' },
+
+	};
+	displaymap(rows, cols, board);
+}
+
+void displaymap(int row, int col, char board[10][10])
+{
+	system("cls");
+	string div = "=====================";
+	string between = "---------------------";
+	cout << div << endl;
+	for (int i = 0; i < row; i++)
+	{
+		//used to divide cells
+		cout << "|";
+		for (int j = 0; j < col; j++)
+		{
+			//Display contents of array and a divider between each cell
+			cout << board[i][j] << "|";
+		}
+		//i dont want to display this if this is my last row because I will display div instead
+		if (i != row - 1)
+		{
+			//Divider between rows
+			cout << endl << between << endl;
+		}
+		else
+		{
+			cout << endl;
+		}
+
+	}
+	cout << div << endl;
+}
+
+
+
+void basement()
+{
+	Sleep(2000);
+	system("cls");
+	cout << "Hello" << endl;
+}
